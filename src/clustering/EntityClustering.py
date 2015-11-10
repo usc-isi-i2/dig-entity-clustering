1db17bb9a80b79f3270627095deaac47b5bb3d4c
Brian Amanatullah <bamanatullah@inferlink.com>
2015-11-09 22:26:47 -0500

    Scaffolding for entity clustering scoring.

--- src/clustering/EntityClustering.py
+++ src/clustering/EntityClustering.py
@@ -2,6 +2,131 @@ import codecs
 import sys
 import getopt
 import json
+from random import random
+
+# personAge, hairColor, eyeColor, name, imageId??
+
+PRIOR = 0.6
+
+def get_best_record_and_score(cluster, records):
+    best_record = {}
+    best_score = 0
+    for record in records:
+        score = 1.0
+        multiplier = 0
+        for key in cluster.get_keys():
+            if key in record:
+                if score_record(cluster, record, key) == 1:
+                    return (record, 1)
+#                 score = score * score_record(cluster, record, key)
+#                 multiplier = 1.0
+        
+        score = score * multiplier
+        if score > best_score:
+            best_record = record
+            best_score = score
+    return (best_record, best_score)
+
+def score_record(cluster, record, unit):
+    best_score = 0.2
+    
+    record_values = []
+    entity_values = cluster.entity[unit]
+    
+    if isinstance(record[unit], basestring):
+        record_values.append(record[unit])
+    else:
+        record_values = record[unit]
+    
+    if unit == 'name':
+        for item in record_values:
+            if item in entity_values:
+                best_score = 1
+                break
+            
+    return best_score
+
+class Cluster(object):
+    def addItem(self, item):
+        self.entity['uri'].append(item['uri'])
+        if len(self.items) == 0:
+            if 'personAge' in item:
+                self.entity['personAge'] = []
+                if isinstance(item['personAge'], basestring):
+                    self.entity['personAge'].append(item['personAge'])
+                else:
+                    self.entity['personAge'].extend(item['personAge'])
+            if 'hairColor' in item:
+                self.entity['hairColor'] = []
+                if isinstance(item['hairColor'], basestring):
+                    self.entity['hairColor'].append(item['hairColor'])
+                else:
+                    self.entity['hairColor'].extend(item['hairColor'])
+            if 'eyeColor' in item:
+                self.entity['eyeColor'] = []
+                if isinstance(item['eyeColor'], basestring):
+                    self.entity['eyeColor'].append(item['eyeColor'])
+                else:
+                    self.entity['eyeColor'].extend(item['eyeColor'])
+            if 'name' in item:
+                self.entity['name'] = []
+                if isinstance(item['name'], basestring):
+                    self.entity['name'].append(item['name'])
+                else:
+                    self.entity['name'].extend(item['name'])
+        else:
+            if 'personAge' in item:
+                if 'personAge' not in self.entity:
+                    self.entity['personAge'] = []
+                
+                if isinstance(item['personAge'], basestring):
+                    self.entity['personAge'].append(item['personAge'])
+                else:
+                    self.entity['personAge'].extend(item['personAge'])
+            if 'hairColor' in item:
+                if 'hairColor' not in self.entity:
+                    self.entity['hairColor'] = []
+                
+                if isinstance(item['hairColor'], basestring):
+                    self.entity['hairColor'].append(item['hairColor'])
+                else:
+                    self.entity['hairColor'].extend(item['hairColor'])
+            if 'eyeColor' in item:
+                if 'eyeColor' not in self.entity:
+                    self.entity['eyeColor'] = []
+                
+                if isinstance(item['eyeColor'], basestring):
+                    self.entity['eyeColor'].append(item['eyeColor'])
+                else:
+                    self.entity['eyeColor'].extend(item['eyeColor'])
+            if 'name' in item:
+                if 'name' not in self.entity:
+                    self.entity['name'] = []
+                    
+                if isinstance(item['name'], basestring):
+                    self.entity['name'].append(item['name'])
+                else:
+                    self.entity['name'].extend(item['name'])
+        
+        self.items.append(item)
+        
+    def get_keys(self):
+        keys = self.entity.keys()
+        keys.remove('uri')
+        return keys
+    
+    def __init__(self):
+        self.items = []
+        self.entity = {}
+        self.entity['uri'] = []
+        
+    def __str__(self, *args, **kwargs):
+        output_object = {}
+        output_object['entity'] = self.entity
+        for item in output_object['entity']:
+            output_object['entity'][item] = list(set(output_object['entity'][item]))
+        output_object['items'] = self.items
+        return json.dumps(output_object)
 
 def main(argv):
     inputfile = ''
@@ -18,11 +143,37 @@ def main(argv):
             inputfile = arg
     print 'Input file is ', inputfile
     
+    records = []
+    
     with codecs.open(inputfile, "r", "utf-8") as myfile:
         for line in myfile:
             the_json = line.encode("utf-8")
             json_object = json.loads(the_json)
-            print json_object
+            records.append(json_object)
+        
+    # Take an item from the canopy and make it a cluster
+    clusters = []
+#     random.shuffle(records)
+    
+    while len(records) > 0:
+        processing_item = records.pop()
+        
+        new_cluster = Cluster()
+        print "Processing " + str(processing_item)
+        new_cluster.addItem(processing_item)
+        score = 1
+        while score > 0.5:
+            (record, score) = get_best_record_and_score(new_cluster, records)
+            if score > 0.5:
+                print score
+                new_cluster.addItem(record)
+                print " -- Added " + str(record)
+                records.remove(record)
+
+        clusters.append(new_cluster)
+        
+        print len(records)
+        print len(clusters)
 
 if __name__ == "__main__":
     main(sys.argv[1:])
